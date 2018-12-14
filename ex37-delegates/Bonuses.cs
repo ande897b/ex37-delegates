64788571e63a1bb2cff8ee5980d73462d698a435
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2018-12-14 09:47:45 +0100

    Add project files.

---
+++ ex37-delegates/Bonuses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex37_delegates
+{
+  public  class Bonuses
+    {
+        public static double TenPercent(double ammount)
+        {
+            return ammount * 0.1;
+        }
+        public static double FlatTwoIfAmountMoreThanFive(double ammount)
+        {
+            double result;
+            if (ammount >=5)
+            {
+                result = 2;
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
---
+++ ex37-delegates/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex37_delegates
+{
+    public delegate double BonusProvider(double ammount);
+  public  class Order
+    {
+
+        public BonusProvider Bonus { get; set; }
+
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct (Product p)
+        {
+            products.Add(p);
+        }
+        public  double GetValueOfProducts()
+
+        {
+            double result = 0;
+            foreach (Product product in products)
+            {
+                result += product.Value;
+            }
+            return result;
+        }
+        public double GetBonus()
+        {
+            double result = 0;
+
+            result = Bonus(GetValueOfProducts());
+
+            return result;
+        }
+        public double GetTotalPrice()
+        {
+            double result = 0;
+
+            result = Bonus(GetValueOfProducts());
+            result = GetValueOfProducts() - result;
+
+            return result;
+        }
+    }
+}
